3c90cbfe18b5098b762473524998816f4a0cca9e
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-08-17 20:52:31 +0300

    start mobile adapt

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
+    // slider arrows
     $('.slider__box').slick({
         prevArrow: '<button class="slider__arrow slider__arrow-left" type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="slider__arrow slider__arrow-right" type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    });
+
+    // burger menu
+    $('.menu-btn').on('click', function() {
+        $('.menu__list').toggleClass('active');
     });
 })
