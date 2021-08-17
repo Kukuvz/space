54ab6fb50efc0063f047d99fc03f0e581e64bc1d
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-08-17 19:46:28 +0300

    add contacts & footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-
+    $('.slider__box').slick({
+        prevArrow: '<button class="slider__arrow slider__arrow-left" type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="slider__arrow slider__arrow-right" type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    });
 })
